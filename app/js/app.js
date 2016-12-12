0c9650cc05afb53df9060d222bef636a5e064d9e
Angela M. Choi <angela.minchoi@gmail.com>
2016-12-11 22:08:01 -0800

    clean up

--- app/js/app.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-window.app = angular.module('portfolio', ['ui.router']);
-
-app.config(function ($urlRouterProvider, $locationProvider) {
-    $locationProvider.html5Mode(true);
-    $urlRouterProvider.otherwise('/');
-});
