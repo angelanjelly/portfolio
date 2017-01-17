7840b5144adc342eb3b471f7a9d32de9c495e5df
Angela M. Choi <angela.minchoi@gmail.com>
2017-01-16 22:11:22 -0800

    added modal img

--- browser/js/app.js
+++ browser/js/app.js
@@ -11,4 +11,39 @@ $(function() {
       }
     }
   });
+  // var modal = $('#myModal');
+  // var modalImg = $('#img01');
+  // var captionText = $('#caption');
+
+  // $('#img').click(function() {
+  //   modal.style.display = 'block';
+  //   modalImg.src = this.src;
+  //   captionText.innerHTML = this.alt;
+  // });
+
+  // $('.close')[0].click(function() {
+  //   modal.style.display = 'none';
+  // })
 });
+
+console.log('hey');
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its 'alt' text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById('img01');
+var captionText = document.getElementById('caption');
+img.onclick = function(){
+  modal.style.display = 'block';
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName('close')[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = 'none';
+}
